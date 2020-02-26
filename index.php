08f5899fb1f1488733f1203b9c0ee4547b2117c4
Esdran26 <vargasandres2627@gmail.com>
2020-02-25 22:00:28 -0500

    Validating no User

--- index.php
+++ index.php
@@ -21,7 +21,30 @@
             <div class="textForm">
                 <div class="messageErrorDiv">
                     <!--<p class="messageError">Error, ese nombre de usuario no existe</p><br>-->
-                    
+                    <?php 
+                        if($_POST) {
+                            function selectUser() {
+                                include_once 'Config/connection.php';
+                            
+                                $sqlSelectUser = 'SELECT us_name FROM users';
+                                $sentenceSelectUser = $pdo->prepare($sqlSelectUser);
+                                $sentenceSelectUser->execute();
+                                $resultSelectUser = $sentenceSelectUser->fetchAll();
+                            
+                                //La variable por defecto va a ser negativa
+                                $messageError = '<p class="messageError">Error, ese nombre de usuario no existe</p>';
+                            
+                                //Si encuentra algún caso, quitará lo negativo
+                                foreach ($resultSelectUser as $result) {
+                                    if($_POST['usUser'] === $result['us_name']) {
+                                        $messageError = '<p style="display: none">Correcto</p>';
+                                    }
+                                }
+                                return $messageError;
+                            }
+                            echo selectUser();
+                        }
+                    ?>
                 </div>
                 <form method="POST" action="index.php">
                     <label id="labelUser" for="inputUser">Nombre de Usuario: </label>

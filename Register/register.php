949aaadb5f487f1d36e728ff128064a3e7458a10
Esdran26 <vargasandres2627@gmail.com>
2020-02-28 15:32:13 -0500

    Create file register.php

---
+++ Register/register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro de Usuarios</title>
+</head>
+<body>
+    <h1>Atención!!!</h1>
+    <p>Este tipo de registro se necesitan únicamente para que la contraseña esté cifrada y que ningún atacante informático pueda tomarla y tener acceso al sistema</p>
+
+    <form action="register.php" method="post">
+        <input type="text" name="" id="">
+    </form>
+</body>
+</html>

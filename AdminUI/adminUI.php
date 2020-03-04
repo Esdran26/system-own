9616b127167ebf4bae45af3ffb13e16398f27ae6
Esdran26 <vargasandres2627@gmail.com>
2020-03-04 16:09:05 -0500

    Adding sidebar in adminUI

--- AdminUI/adminUI.php
+++ AdminUI/adminUI.php
@@ -7,11 +7,24 @@ if(isset($_SESSION['admin'])) { ?>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/de6467cf36.js"></script>
+        <link rel="stylesheet" href="Css/adminUI.css">
         <title>Página de Administración</title>
     </head>
     <body>
-        <h1>Bienvenido <?php echo $_SESSION['admin'] ?></h1>
-        <a href="../logout.php">Cerrar Sesión</a>
+        <div id="sidebar">
+            <ul>
+                <li>
+                <i class="far fa-user-circle fa-4x logo"></i>
+                </li>
+                <li><a href="#">Bienvenido <?php echo $_SESSION['admin'] ?></a></li>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Llamar a caja</a></li>
+                <li><a href="#">Registrar</a></li>
+                <li><a href="#">Modificar o eliminar</a></li>
+            </ul>
+        </div>
     </body>
     </html>
 <?php
